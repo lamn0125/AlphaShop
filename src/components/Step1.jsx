b7ec67002d84df4d482ccfe0f66b71d17c4846e0
lamn0125 <lamn0125@gmail.com>
2023-05-05 16:46:47 +0800

    rewrite components Step1 select options (use mapping)

--- src/components/Step1.jsx
+++ src/components/Step1.jsx
@@ -10,6 +10,125 @@ function Step1Input({ label, placeholder }) {
 }
 
 export default function Step1() {
+  const cities = [
+    {
+      name: "基隆市",
+      value: "KLU",
+    },
+    {
+      name: "新北市",
+      value: "TPH",
+    },
+    {
+      name: "臺北市",
+      value: "TPE",
+    },
+    {
+      name: "桃園市",
+      value: "TYC",
+    },
+    {
+      name: "新竹縣",
+      value: "HSH",
+    },
+    {
+      name: "新竹市",
+      value: "HSC",
+    },
+    {
+      name: "苗栗市",
+      value: "MAC",
+    },
+    {
+      name: "苗栗縣",
+      value: "MAL",
+    },
+    {
+      name: "臺中市",
+      value: "TXG",
+    },
+    {
+      name: "彰化縣",
+      value: "CWH",
+    },
+    {
+      name: "彰化市",
+      value: "CWS",
+    },
+    {
+      name: "南投市",
+      value: "NTC",
+    },
+    {
+      name: "南投縣",
+      value: "NTO",
+    },
+    {
+      name: "雲林縣",
+      value: "YLH",
+    },
+    {
+      name: "嘉義縣",
+      value: "CHY",
+    },
+    {
+      name: "嘉義市",
+      value: "CYI",
+    },
+    {
+      name: "臺南市",
+      value: "TNN",
+    },
+    {
+      name: "高雄市",
+      value: "KHH",
+    },
+    {
+      name: "屏東縣",
+      value: "IUH",
+    },
+    {
+      name: "屏東市",
+      value: "PTS",
+    },
+    {
+      name: "宜蘭縣",
+      value: "ILN",
+    },
+    {
+      name: "宜蘭市",
+      value: "ILC",
+    },
+    {
+      name: "花蓮縣",
+      value: "HWA",
+    },
+    {
+      name: "花蓮市",
+      value: "HWC",
+    },
+    {
+      name: "臺東市",
+      value: "TTC",
+    },
+    {
+      name: "臺東縣",
+      value: "TTT",
+    },
+    {
+      name: "澎湖縣",
+      value: "PEH",
+    },
+    {
+      name: "金門縣",
+      value: "KMN",
+    },
+    {
+      name: "連江縣",
+      value: "LNN",
+    },
+  ];
+
   return (
     <form className={styles.address} data-phase="address">
       <h3 className={styles.formTitle}>寄送地址</h3>
@@ -39,35 +158,11 @@ export default function Step1() {
             <div className={styles.selectContainer}>
               <select required>
                 <option value="">請選擇縣市</option>
-                <option value="KLU">基隆市</option>
-                <option value="TPH">新北市</option>
-                <option value="TPE">臺北市</option>
-                <option value="TYC">桃園市</option>
-                <option value="HSH">新竹縣</option>
-                <option value="HSC">新竹市</option>
-                <option value="MAC">苗栗市</option>
-                <option value="MAL">苗栗縣</option>
-                <option value="TXG">臺中市</option>
-                <option value="CWH">彰化縣</option>
-                <option value="CWS">彰化市</option>
-                <option value="NTC">南投市</option>
-                <option value="NTO">南投縣</option>
-                <option value="YLH">雲林縣</option>
-                <option value="CHY">嘉義縣</option>
-                <option value="CYI">嘉義市</option>
-                <option value="TNN">臺南市</option>
-                <option value="KHH">高雄市</option>
-                <option value="IUH">屏東縣</option>
-                <option value="PTS">屏東市</option>
-                <option value="ILN">宜蘭縣</option>
-                <option value="ILC">宜蘭市</option>
-                <option value="HWA">花蓮縣</option>
-                <option value="HWC">花蓮市</option>
-                <option value="TTC">臺東市</option>
-                <option value="TTT">臺東縣</option>
-                <option value="PEH">澎湖縣</option>
-                <option value="KMN">金門縣</option>
-                <option value="LNN">連江縣</option>
+                {cities.map((city) => (
+                  <option value={city.value} key={city.value}>
+                    {city.name}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
